e5066ee04fb46bc4e5f083c41a7206e7c349de48
Nyoman Pastika <113783677+passchatter@users.noreply.github.com>
2024-11-27 14:28:45 +0800

    firs commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    container:{
+      center:true,
+      padding:"1rem"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
